3900eb1cc94a2e3b21bdd2d1f0914996e53a3b91
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-29 19:42:46 +0700

    Add encrypt function

---
+++ caesarCipher.js
@@ -0,0 +1,38 @@
+function caesarCipher(text, key) {
+
+}
+
+function shiftChar(key) {
+	return key % 26;
+}
+
+function encrypt(text, key) {
+	let string = text.split('');
+
+	let encryptText = '';
+	for (let char in string) {
+		// char code
+		// a -> 97 ... z -> 122
+		// A -> 65 ... Z -> 90
+		let ch = text.charCodeAt(char);
+
+		// shift alphabet only
+		// Shift lowercase
+		if (ch >= 97 && ch <= 122) {
+			ch += shiftChar(key);
+			if (ch > 122) ch -= 26;
+		}
+
+		// Shift uppercase
+		else if (ch >= 65 && ch <= 90) {
+			ch += shiftChar(key);
+			if (ch > 90) ch -= 26;
+		}
+
+		encryptText += String.fromCharCode(ch);
+	}
+	console.log(encryptText);
+	return encryptText;
+}
+
+encrypt('Hello, World!', 1);
